c8e803edd69ece6e1008a51632da80fd584301cd
Clayder <peterclayder@gmail.com>
2023-08-21 21:26:47 -0300

    init

---
+++ src/test/kotlin/com/clayder/api/comabemdelivery/ComaBemDeliveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clayder.api.comabemdelivery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ComaBemDeliveryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

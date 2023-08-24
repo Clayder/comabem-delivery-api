40d6113a732c97c982ee6ce86e43451fe6fd4675
Clayder <peterclayder@gmail.com>
2023-08-23 21:58:15 -0300

    endpoint create and update Kitchen

--- src/main/kotlin/com/clayder/api/comabemdelivery/api/controller/KitchenController.kt
+++ src/main/kotlin/com/clayder/api/comabemdelivery/api/controller/KitchenController.kt
@@ -2,10 +2,18 @@ package com.clayder.api.comabemdelivery.api.controller
 
 import com.clayder.api.comabemdelivery.domain.model.KitchenModel
 import com.clayder.api.comabemdelivery.domain.repository.KitchenRepository
+import org.springframework.beans.BeanUtils
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/kitchen")
@@ -17,7 +25,34 @@ class KitchenController(private val repository: KitchenRepository ) {
     }
 
     @GetMapping("/{id}")
-    fun getById(@PathVariable id: Long): KitchenModel{
-        return repository.findById(id).orElseGet(null)
+    fun getById(@PathVariable id: Long): ResponseEntity<KitchenModel> {
+
+        val kitchen: Optional<KitchenModel> = repository.findById(id)
+
+        if (kitchen.isEmpty) {
+            return ResponseEntity.notFound().build()
+        }
+
+        return ResponseEntity.ok(kitchen.get())
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody kitchenModel: KitchenModel): KitchenModel {
+        return repository.save(kitchenModel)
+    }
+
+    @PutMapping("/{id}")
+    fun update(@PathVariable id: Long, @RequestBody kitchenUpdate: KitchenModel): ResponseEntity<KitchenModel> {
+
+        val kitchen: Optional<KitchenModel> = repository.findById(id)
+
+        if (kitchen.isEmpty) {
+            return ResponseEntity.notFound().build()
+        }
+
+        kitchen.get().name = kitchenUpdate.name
+
+        return ResponseEntity.ok(repository.save(kitchen.get()))
     }
 }

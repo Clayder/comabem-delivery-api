c7b30b880c10ec6081b1c08036607790af1629b5
Clayder <peterclayder@gmail.com>
2023-08-30 19:42:54 -0300

    used @Query repository

--- src/main/kotlin/com/clayder/api/comabemdelivery/domain/repository/RestaurantRepository.kt
+++ src/main/kotlin/com/clayder/api/comabemdelivery/domain/repository/RestaurantRepository.kt
@@ -2,8 +2,13 @@ package com.clayder.api.comabemdelivery.domain.repository
 
 import com.clayder.api.comabemdelivery.domain.model.RestaurantModel
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface RestaurantRepository: JpaRepository<RestaurantModel, Long> {
+interface RestaurantRepository: JpaRepository<RestaurantModel, Long>, IRestaurantQueryRepository {
+
+    @Query("FROM RestaurantModel WHERE name LIKE %:name% AND kitchen.id = :id")
+    fun findRestaurantsByName(name: String, @Param("id") kitchenId: Long): List<RestaurantModel>
 }
